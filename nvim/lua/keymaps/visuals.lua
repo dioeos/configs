6d82c9351d7bdc01bdac62218f474a859c367e70
dio <er.dioneda@gmail.com>
2026-05-19 13:35:41 -0400

    nvim: Changed cursor smear to be native ghostty shader

--- nvim/lua/keymaps/visuals.lua
+++ nvim/lua/keymaps/visuals.lua
@@ -1,5 +1,8 @@
 local M = {}
 function M.setup()
   vim.keymap.set("n", "<leader>st", ":Themery<CR>")
+
+  vim.keymap.set("n", "<leader>te", ":TransparentToggle<CR>")
+
 end
 return M
--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -28,16 +28,6 @@ return {
     end
   },
   {
-    "sphamba/smear-cursor.nvim",
-    config = function()
-      require("smear_cursor").setup({
-        stiffness = 0.8,
-        trailing_stiffness = 0.6,
-        stiffness_insert_mode = 0.7,
-        damping = 0.95,
-        damping_insert_mode = 0.95,
-        distance_stop_animating = 0.5,
-      })
-    end
-  },
+    "xiyaowong/transparent.nvim"
+  }
 }
