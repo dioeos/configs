aaa7aac5a89553edc9b107d0e854b3dd7539ca6e
dio <er.dioneda@gmail.com>
2026-07-03 09:06:46 -0600

    nvim: Fix nixcat LSP

--- nixos-config/modules/home-manager/nixcats/nvim/lua/config/lsp/diagnostics.lua
+++ nixos-config/modules/home-manager/nixcats/nvim/lua/config/lsp/diagnostics.lua
@@ -1,7 +1,22 @@
+vim.opt.signcolumn = "yes"
+
 vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+  },
+
   virtual_text = {
     spacing = 4,
-    source = "if_many",
+    source = "always",
     prefix = "● ",
   },
+  severity_sort = true,
+  float = {
+    source = "always"
+  }
 })
