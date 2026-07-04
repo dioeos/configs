911fbfdd73aa1027cb633915d627bd1c01070e38
dio <er.dioneda@gmail.com>
2026-07-03 18:46:35 -0600

    fix: Add systemd for awww for proper wallpaper set on start

--- nixos-config/modules/home-manager/awww/awww-config.nix
+++ nixos-config/modules/home-manager/awww/awww-config.nix
@@ -1,7 +1,45 @@
 { pkgs, ... }:
 
+let
+  wallpaperConfig = builtins.fromJSON (builtins.readFile ../../../../merle-config.json);
+in
 {
   home.packages = with pkgs; [
     awww
   ];
+
+  systemd.user.services.awww-daemon = {
+    Unit = {
+      Description = "Awww daemon systemd service for wallpaper functionality";
+    };
+
+    Service = {
+      ExecStart = "${pkgs.awww}/bin/awww-daemon";
+      Restart = "on-failure";
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
+
+  systemd.user.services.awww-wallpaper = {
+    Unit = {
+      Description = "Set startup wallpaper with awww";
+      After = [ "awww-daemon.service" ];
+      Requires = [ "awww-daemon.service" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      Type = "oneshot";
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";
+      ExecStart = "${pkgs.awww}/bin/awww img ${wallpaperConfig.wallpaper}";
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+
+  };
 }
